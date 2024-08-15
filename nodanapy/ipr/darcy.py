e567bd95cb9be19ede1593034c925e56603c720a
Ever J. Ramos I. <everramosisla@gmail.com>
2024-08-15 14:47:13 -0400

    feat: modified archive "darcy.py" refs: #6

--- nodanapy/ipr/darcy.py
+++ nodanapy/ipr/darcy.py
@@ -6,26 +6,35 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import numpy as np
 from _properties.gasProperties import GasProperties
 
-class Darcy(GasProperties):
-    def __init__(self, pressure, temperature, specific_gravity, permeability_g, skin, h, rw, re, num_pressure: int=25):
-        super().__init__(pressure, temperature, specific_gravity)
+class Darcy: 
+    def __init__(self, pressure: int|float, temperature: int|float, specific_gravity: float=0.65, permeability_g: int|float=10, skin: int|float=0, height_formation: int|float=10, well_radius: int|float=0.35, external_radius: int|float=1000):
+        self.pressure = pressure
+        self.temperature = temperature
+        self.specific_gravity = specific_gravity
         self.permeability_g = permeability_g
         self.skin = skin
-        self.h = h
-        self.rw = rw
-        self.re = re
-        self.num_pressure = num_pressure
+        self.height_formation = height_formation
+        self.well_radius = well_radius
+        self.external_radius = external_radius
         
-    def _p_diff_(self):
-        return np.linspace(self.pressure, 14.7, self.num_pressure)
+    def _properties_gas_(self):
+        properties = GasProperties(self.pressure, self.temperature, self.specific_gravity)
+        mu_gas = properties.viscosity_gas()
+        z_gas = properties.factor_compressibility_gas()
+        return [mu_gas, z_gas]
+        
+    def _p_diff_(self, range: int=25):
+        return np.linspace(self.pressure, 14.7, range)
         
     def _a_flow_(self):
-        A = ((1424*self.viscosity_gas()*self.factor_compressibility_gas()*self.temperature)/(self.permeability_g*self.h))*(np.log(0.472*(self.re/self.rw))+self.skin)
+        mu, z = self._properties_gas_()
+        A = ((1424*mu*z*self.temperature)/(self.permeability_g*self.height_formation))*(np.log(0.472*(self.external_radius/self.well_radius))+self.skin)
         return A
         
     def _b_flow_(self): 
+        z = self._properties_gas_()[1]        
         beta = (2.33*1e10/(self.permeability_g**1.201))
-        B = (3.16e-12*beta*self.specific_gravity*self.factor_compressibility_gas()*self.temperature)/((self.h**2)*self.rw)
+        B = (3.16e-12*beta*self.specific_gravity*z*self.temperature)/((self.height_formation**2)*self.well_radius)
         return B
         
     def flow_gas(self):
@@ -40,19 +49,24 @@ class Darcy(GasProperties):
         
         return np.array(flow_results)
     
+    def darcy(self, range: int=25):
+        qg = self.flow_gas()
+        pwf = self._p_diff_(range=range)
+        return [qg, pwf]
     
-# ipr1 = Darcy(1309, 600, 0.673, 11, 9.95, 76, 0.3542, 909)
+#if __name__ == "__main__":
+    
+#    ipr1 = Darcy(1309, 600)
 
-# p = ipr1._p_diff_()
-# q = ipr1.flow_gas()
-# print(q)
-# import matplotlib.pyplot as plt
+#     p = ipr1._p_diff_()
+#     q = ipr1.flow_gas()
+#    total = ipr1.darcy(10)
+#    print(total)
+    # import matplotlib.pyplot as plt
 
-# plt.plot(q, p)
-# plt.title('IPR')
-# plt.ylabel('Pwf(psia)')
-# plt.xlabel('Qg(Mscf/D)')
-# plt.show()
-
-
+    # plt.plot(q, p)
+    # plt.title('IPR')
+    # plt.ylabel('Pwf(psia)')
+    # plt.xlabel('Qg(Mscf/D)')
+    # plt.show()
 
