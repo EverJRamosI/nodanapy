3b954583200258d01d780e278530192f935a68c2
Ever J. Ramos I. <everramosisla@gmail.com>
2024-09-03 18:41:24 -0400

    feat: modify file 'beggsBrill.py' refs: #12

--- nodanapy/vlp/beggsBrill.py
+++ nodanapy/vlp/beggsBrill.py
@@ -41,7 +41,7 @@ class BeggsBrill:
         mu_oil = properties.viscosity_oil()
         rho_oil = properties.density_oil()
         sigma_oil = properties.tension_oil()
-        if sigma_oil < 1:
+        if sigma_oil < 0:
             sigma_oil = np.array(1)
         rs_oil = properties.solution_oil()
         fvf_oil = properties.factor_volumetric_oil()
@@ -52,56 +52,52 @@ class BeggsBrill:
         mu_water = properties.viscosity_water()
         rho_water = properties.density_water()
         sigma_water = properties.tension_water()
-        if sigma_water < 1:
+        if sigma_water < 0:
             sigma_water = np.array(1)
         rs_water = properties.solution_water()
         fvf_water = properties.factor_volumetric_water()
         return [mu_water, rho_water, sigma_water, rs_water, fvf_water]
     
     def _properties_liquid_(self):
+        mu_o, rho_o, sigma_o, rs_o, bo = self._properties_oil_()
+        mu_w, rho_w, sigma_w, rs_w, bw = self._properties_water_()
         WOR = self.water_cut/(1-self.water_cut)
-        rho_liq = self._properties_oil_()[1]*(1/(1+WOR)) + self._properties_water_()[1]*(WOR/(1+WOR))
-        mu_liq = self._properties_oil_()[0]*(1/(1+WOR)) + self._properties_water_()[0]*(WOR/(1+WOR))
-        sig_liq = self._properties_oil_()[2]*(1/(1+WOR)) + self._properties_water_()[2]*(WOR/(1+WOR))
+        rho_liq = rho_o*(1/(1+WOR)) + rho_w*(WOR/(1+WOR))
+        mu_liq = mu_o*(1/(1+WOR)) + mu_w*(WOR/(1+WOR))
+        sig_liq = sigma_o*(1/(1+WOR)) + sigma_w*(WOR/(1+WOR))
         return [rho_liq, mu_liq, sig_liq]
-        
     
-    # def _temp_gradient_(self, bh_temperature):
-    #     self.bh_temperature = bh_temperature
-    #     return np.abs((self.temperature-self.bh_temperature))/self.well_depth
-    
-    
-    def _flow_(self, qo=0.0001):
+    def _flow_(self, qo=0.1):
         self.qo = qo
+        *_, rs_o, bo = self._properties_oil_()
+        *_, rs_w, bw = self._properties_water_()
+        *_, bg, _ = self._properties_gas_()
         WOR = self.water_cut/(1-self.water_cut)
         #qo = np.linspace(1, self.qo_max, self.amount)
-        q_oil = (self._properties_oil_()[4]*self.qo)/15387
-        q_water = (self._properties_water_()[3]*WOR*self.qo)/15387
+        q_oil = (bo*self.qo)/15387
+        q_water = (bw*WOR*self.qo)/15387
         q_liq = q_oil+q_water
-        if (self.go_ratio - self._properties_oil_()[3]) < 0:
+        if (self.go_ratio - rs_o) < 0:
             q_gas = 0
         else:
-            q_gas = self._properties_gas_()[2]*(self.go_ratio-self._properties_oil_()[3]-(self._properties_water_()[3]*WOR))*self.qo/86400
+            q_gas = bg*(self.go_ratio-rs_o-(rs_w*WOR))*self.qo/86400
         
-        # q_lm = ((self.go_ratio*(self.qg_max*1.2))/(1-self.water_cut))
-        # q_liq = np.linspace(1, q_lm, self.amount)
-        # q_oil = (1-self.water_cut)*q_liq
-        # q_water = q_liq-q_oil
-        # lgr = self.go_ratio/(1-self.water_cut)
-        # q_gas = (q_liq)/lgr
         return [q_water, q_oil, q_gas, q_liq]
     
     def _velocities_(self):
+        *_, qg, ql = self._flow_(self.qo)
         area = np.pi/4*((self.internal_diameter/12)**2)
-        v_sl = self._flow_()[3]/area
-        v_sg = self._flow_()[2]/area
+        v_sl = ql/area
+        v_sg = qg/area
         v_m = v_sl + v_sg
         return [v_sl, v_sg, v_m]
     
     def _flow_regime_(self):
-        Nfr = (self._velocities_()[2]**2)/((self.internal_diameter/12)*32.174)
-        Nlv = 1.938*self._velocities_()[0]*((self._properties_liquid_()[0]/self._properties_liquid_()[2])**0.25)
-        lambda_liq = self._velocities_()[0] / self._velocities_()[2]
+        rho_l, _, sig_l = self._properties_liquid_()
+        vsl, _, vm = self._velocities_()
+        Nfr = (vm**2)/((self.internal_diameter/12)*32.174)
+        Nlv = 1.938*vsl*((rho_l/sig_l)**0.25)
+        lambda_liq = vsl / vm
         lambda_gas = 1-lambda_liq
         L1 = 316*(lambda_liq**0.302)
         L2 = 9.252e-4*(lambda_liq**(-2.4684))
@@ -121,18 +117,13 @@ class BeggsBrill:
                 return 'intermittent flow'
             elif ((lambl_v<0.4 and nfr_v>=l1_v) or (lambl_v>=0.4 and nfr_v>l4_v)):
                 return 'distributed flow'
-        
         #regime = [reg(nfr_val, lamb_val, l1_val, l2_val, l3_val, l4_val) for nfr_val, lamb_val, l1_val, l2_val, l3_val, l4_val in zip(nfr, lambl, l1, l2, l3, l4)]
         regime = reg(nfr, lambl, l1, l2, l3, l4)
         return regime
     
     def holdup_liq(self):
         regime = self._regime_()
-        nfr = self._flow_regime_()[0]
-        nlv = self._flow_regime_()[1]
-        l2 = self._flow_regime_()[3]
-        l3 = self._flow_regime_()[4]
-        lamb_liq = self._flow_regime_()[6]
+        nfr, nlv, _, l2, l3, _, lamb_liq, _ = self._flow_regime_()
         angle_value = (self.angle*np.pi)/180
         
         def hold_l(nfr_v, nlv_v, lambl_v, regime_v, l2_v, l3_v):
@@ -171,11 +162,11 @@ class BeggsBrill:
                 
             if hold_liq_h < lambl_v:
                 hold_liq_h = lambl_v
-                
+            #if hold_liq_h > 1:
+            #    hold_liq_h = 1
             holdup_liq = hold_liq_h*psi
             
             return holdup_liq
-        
         #holdup_liquid = np.array([hold_l(nfr_val, nlv_val, lambl_val, regime_val, l2_val, l3_val) for nfr_val, nlv_val, lambl_val, regime_val, l2_val, l3_val in zip(nfr, nlv, lamb_liq, regime, l2, l3)])
         holdup_liquid = hold_l(nfr, nlv, lamb_liq, regime, l2, l3)
         holdup_gas = 1 - holdup_liquid
@@ -183,8 +174,7 @@ class BeggsBrill:
         return [holdup_liquid, holdup_gas]        
         
     def _properties_mixture_(self):
-        lmbd_l = self._flow_regime_()[-2]
-        lmbd_g = self._flow_regime_()[-1]
+        *_, lmbd_l, lmbd_g = self._flow_regime_()
         h_l, h_g = self.holdup_liq()
         rho_l, mu_l, _ = self._properties_liquid_()
         mu_g, rho_g, *_ = self._properties_gas_()
@@ -195,9 +185,11 @@ class BeggsBrill:
     
     def _number_reynolds_(self, rugosity=0.0001):
         self.rugosity = rugosity
+        *_, v_m = self._velocities_()
+        rho_m, mu_m, _ = self._properties_mixture_()
         lmbd_l = self._flow_regime_()[-2]
         h_l = self.holdup_liq()[0]
-        NRe = (1488*self._properties_mixture_()[0]*self._velocities_()[-1]*(self.internal_diameter/12))/(self._properties_mixture_()[1])
+        NRe = (1488*rho_m*v_m*(self.internal_diameter/12))/(mu_m)
         fn = (-2*np.log10((1/3.7)*(self.rugosity/self.internal_diameter))+((6.81/NRe)**0.9))**(-2)
         y = lmbd_l/(h_l**2)
         
@@ -208,7 +200,6 @@ class BeggsBrill:
                 s = np.log(y_v)/((-0.0523) + (3.182*np.log(y_v)) - (0.8725*((np.log(y_v))**2)) + (0.01853*((np.log(y_v))**4)))
 
             return fn_v*np.exp(s)
-        
         #ft = np.array([factor_t(y_val, fn_val) for y_val, fn_val in zip(y, fn)])
         ft = factor_t(y, fn)
         return [NRe, ft]
@@ -217,7 +208,6 @@ class BeggsBrill:
         angle_value = (self.angle*np.pi)/180
         rho_mis = self._properties_mixture_()[-1]
         delta_pressure = (rho_mis*np.sin(angle_value))/144
-        #print('potential', delta_pressure)
         return delta_pressure
     
     def pressure_drop_friction(self):
@@ -225,15 +215,12 @@ class BeggsBrill:
         v_m = self._velocities_()[2]
         ft = self._number_reynolds_()[1]
         delta_pressure = 2*ft*rho_mis*(v_m**2) / 32.17 / (self.internal_diameter/12) / 144
-        #print("friction", delta_pressure)
         return delta_pressure
     
     def kinetic_factor(self):
-        v_sg = self._velocities_()[1]
-        v_m = self._velocities_()[2]
+        v_sl, v_sg, v_m = self._velocities_()
         rho_mis = self._properties_mixture_()[-1]
         Ek = v_m*v_sg*rho_mis / 32.17 / self.pressure / 144
-        #print('kinetic', Ek)
         return Ek
     
     def pressure_drop_total(self):
@@ -248,98 +235,16 @@ class BeggsBrill:
         gradient = np.abs((self.temperature-self.bh_temperature))/self.well_depth
         return self.temperature + gradient*self._delta_depth_()
     
-    def _delta_flow_(self, qo=0.0001):
-        self.qo = qo
-        q_oil = np.linspace(self.qo, self.qo_max, self.amount)
+    def _delta_flow_(self, qo=0.1):
+        #self.qo = qo
+        q_oil = np.linspace(qo, self.qo_max, self.amount)
         return q_oil
-        # print(qo)
-        # Qw, Qo, Qg, Ql = self._flow_()
-        
-        # print(Qw, Qo, Qg, Ql)
-        
-    # def pressure_traverse(self):
-    #     pi = []
-    #     dpdz = []
-    #     dt = self._delta_temp_(self.bh_temperature)
-    #     for i in range(self.amount):
-    #         if i == 0:
-    #             pi.append(self.temperature)
-    #         else:
-    #             dz = (self._delta_depth_()[i]-self._delta_depth_()[i-1])
-    #             pressure_i = pi[i-1] + dz*dpdz[i-1]
-    #             pi.append(pressure_i)
-            
-    #         dpdz_i_cla = BeggsBrill(pi[i], dt[i], self.specific_gravity, self.api, self.bubble_pressure,
-    #                             self.salinity, self.water_cut, self.go_ratio, self.internal_diameter, self.rugosity, self.well_depth,
-    #                             self.angle, self.qo_max, self.amount)
-    #         dpdz_i = dpdz_i_cla.pressure_drop_total()
-    #         dpdz.append(dpdz_i)
-            
-    #     return [np.array(pi), np.array(dpdz)]
-    #################################################
-    # def pressure_traverse(self):
-    #     pn = self.pressure
-    #     dT = self._delta_temp_()
-    #     dH = self._delta_depth_()
-    #     error = 1e-3
-        
-    #     p = []
-    #     dpt = []
-        
-    #     for temp, i in zip(dT, range(len(dH))):
-    #         self.temperature = temp
-    #         #print(self.temperature)
-            
-    #         if temp == dT[0]:
-    #             p.append(pn)
-    #             dp0 = self.pressure_drop_total()
-    #             #print(dp0)
-    #             dpt.append(dp0)
-    #             continue
-            
-    #         dz = dH[i] - dH[i-1]
-            
-    #         dP = dpt[i-1]
-    #         pi1 = dP*dz + p[i-1]
-    #         self.pressure = pi1
-    #         dP_1 = self.pressure_drop_total()
-    #         #print(dP, pi1, self.pressure, dP_1)
-            
-    #         while True:
-                
-    #             dP = dP_1
-    #             pi = dP*dz + p[i-1]
-    #             self.pressure = pi
-    #             dP_n = self.pressure_drop_total()
-                
-    #             pj = dP_n*dz + p[i-1]
-    #             self.pressure = pj
-                
-    #             #print(pj)
-    #             if np.abs(dP_n - dP) <= error:
-    #                 p.append(pj)
-    #                 dpt.append(dP_n)
-    #                 #dP = dP_n
-    #                 #print(p)
-    #                 #print(dpt)
-    #                 break
-                
-    #             dP_1 = dP_n
-    #             #pn = self.pressure
-                
-    #             #print(dP)
-    #             #print(pn)
-        
-    #     return [np.array(p), np.array(dpt), dT, dH]
-    ####################################
     
-    def _pressure_traverse_iter_(self, amount=2):
-        self.amount = amount
+    def pressure_traverse(self):
         pn = self.pressure
         dT = self._delta_temp_()
         dH = self._delta_depth_()
-        error = 1e-5
-
+        
         p = [pn]
         dpt = [self.pressure_drop_total()]
 
@@ -348,134 +253,41 @@ class BeggsBrill:
         for i, dz in enumerate(dz_array, 1):
             self.temperature = dT[i]
 
-            pi1 = dpt[i-1] * dz + p[i-1]
-            self.pressure = pi1
-            dP_1 = self.pressure_drop_total()
-
-            while True:
-                dP = dP_1
-                pi = dP * dz + p[i-1]
-                self.pressure = pi
-                dP_n = self.pressure_drop_total()
-
-                if np.abs(dP_n - dP) <= error:
-                    p.append(pi)
-                    dpt.append(dP_n)
-                    break
-                
-                dP_1 = dP_n
-
+            pi = dpt[i-1] * dz + p[i-1]
+            self.pressure = pi
+            dP_n = self.pressure_drop_total()
+            
+            p.append(pi)
+            dpt.append(dP_n)
+            
+        self.temperature = dT[0]
+        self.pressure = pn
+        
         return [np.array(p), np.array(dpt), dT, dH]
-
-        #dP = pn/2
-        #pi = dP + pn
-        #
-        #self.pressure = pi
-        #self.temperature = dT[1]
-        #
-        #dP_1 = self.pressure_drop_total()
             
+    def outflow(self):
+        qon = self._delta_flow_()    
+        qoi = []
+        qwi = []
+        qgi = []
+        qli = []
+        pwf_list = []
         
-        # print(dP_1)
-        # pi = dP_1 + pn
+        for flow_value in qon:
+            q_w, q_o, q_g, q_l = self._flow_(flow_value)
+            pwf, *_ = self.pressure_traverse()
+            pwf_list.append(pwf[-1])
+            qoi.append(q_o*15387)
+            qwi.append(q_w*15387)
+            qgi.append(q_g*86400)
+            qli.append(q_l*15387)
+            #ql, qg, qo, qw, pwf
+        return [np.array(qli), np.array(qgi), np.array(qoi), np.array(qwi), np.array(pwf_list)]
         
-        # self.pressure = pi
-        
-        # dP_2 = self.pressure_drop_total()
-        # print(dP_2)
-        
-        # pi = dP_2 + pn
-        # self.pressure = pi
-        
-        # dP_3 = self.pressure_drop_total()
-        # print(dP_3)
-        
-        # pi = dP_3 + pn
-        # self.pressure = pi
-        
-        # dP_4 = self.pressure_drop_total()
-        # print(dP_4)
-        
-        # error = 1e-10
-        
-        # if np.abs(dP-dP_1) >= error:
-        #     print(dP)
-        #     print(dP_1)
-        #     print(dP-dP_1)
             
-        
-        # print(self.pressure, self.temperature)
-        
-        # print(dP)
-        # print(dP_1)
-        
-        # def drop_pressure(p, t, sg, api, pb, sal, wc, gor, id, h, ang, qom, am):
-        #     drop = BeggsBrill(p, t, sg, api, pb, sal, wc, gor, id, h, ang, qom, am)
-        #     dp_total = drop.pressure_drop_total()
-        #     return dp_total
-        # #dp_i = drop_pressure(pi, dT[0], self.specific_gravity, self.api, self.bubble_pressure, self.salinity, self.water_cut, self.go_ratio, self.internal_diameter, self.well_depth, self.angle, self.qo_max, self.amount)
-        # #print(dp_i)
-        # for i in range(len(dH)):
-        #     dz = dH[i]-dH[i-1]
-        #     dt = dT[i]-dT[i-1]
-        #     #print(dz, dt)
-            
-        
-        #print(dT, dH, dP, pi, qo)
-        
-    
-    # print(reg)
-    # def interpolate(regime, nfr, nlv, l2, l3):
-    #     pass
-    
-    # def holdup_liq(nfr, nlv, lamb_liq, angle, regime):
-    #     pass
-            
-def pressure(specific_gravity: float=0.65, 
-            api: int|float=35, bubble_pressure: int|float=1500, salinity: int|float=1000, water_cut: float=0.67, 
-            go_ratio: int|float=300, internal_diameter: int|float=2.5, well_depth: int|float=5000, 
-            angle: int|float=90, qo_max: int|float=1000, amount: int=25):
-    
-    thp = (150+460)
-    tht = (100+460)
-    twf = (150+460)
-    
-    def gradient(t0, t1, depth):
-        if depth == 0:
-            return 0
-        else:
-            return abs(t0-t1)/depth
-    
-    depths = np.linspace(0, well_depth, amount)
-    
-    t_g = gradient(tht, twf, well_depth)
-    
-    temps = tht + t_g*depths
-    
-    p = []
-    dpdz = []
-    
-    for i in range(len(depths)):
-        if i == 0:
-            p.append(thp)
-        else:
-            dz = (depths[i]-depths[i-1])
-            pressure = p[i-1]+dz*dpdz[i-1]
-            #print(pressure)
-            p.append(pressure)
-            
-        well = BeggsBrill(p[i], temps[i], specific_gravity, api, bubble_pressure, salinity, water_cut, go_ratio, internal_diameter, well_depth, angle, qo_max, amount)
-        #print(well._properties_oil_())
-        well._flow_(100)
-        dpdz_step = well.pressure_drop_total()
-        dpdz.append(dpdz_step)
-        
-    return [np.array(p), np.array(dpdz), depths]
-        
-    
 if __name__ == "__main__":
     
-    well = BeggsBrill(450, (100+460), )
+    well = BeggsBrill(450, (100+460), bubble_pressure=1500)
     #well._flow_(500)
     #print(well._delta_temp_())
     #print(well._delta_depth_())
@@ -494,13 +306,17 @@ if __name__ == "__main__":
     
     #print(well.pressure_traverse())
     
-    dp = well._pressure_traverse_iter_()[0]
-    dh = well._pressure_traverse_iter_()[3]
-    print(dp, dh)
-    #import matplotlib.pyplot as plt
+    #dp = well._pressure_traverse_iter_()[0]
+    #dh = well._pressure_traverse_iter_()[3]
+    #print(dp, dh)
+    #q = well._delta_flow_()
+    #p = well.bottom_hole_pressure()
+    #print(q, p)
+    #print(well.outflow())
+    
+    import matplotlib.pyplot as plt
     #
-    #
-    #p, dpdz, dept = pressure()
-    #print(p, dpdz, dept)
-    #plt.plot(dp, dh)
-    #plt.show()
+    ql, qg, qo, qw, pw = well.outflow()
+    print(ql, qg, qo, qw, pw)
+    plt.plot(qo, pw)
+    plt.show()
