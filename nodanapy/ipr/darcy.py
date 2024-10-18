861a8035c8a98c0d464f6e244de1e183fe974c80
Ever J. Ramos I. <everramosisla@gmail.com>
2024-10-17 20:53:34 -0400

    feat: modify 'darcy.py' refs: #28

--- nodanapy/ipr/darcy.py
+++ nodanapy/ipr/darcy.py
@@ -8,14 +8,14 @@ from _properties.gasProperties import GasProperties
 
 class Darcy: 
     def __init__(self, pressure: int|float, temperature: int|float, specific_gravity: float=0.65, 
-                permeability_g: int|float=10, skin: int|float=0, height_formation: int|float=10, 
-                well_radius: int|float=0.35, reservoir_radius: int|float=1000, water_cut: float=0.1, 
+                permeability: int|float=10, skin: int|float=0, height_formation: int|float=10, 
+                well_radius: int|float=0.35, reservoir_radius: int|float=1000, water_cut: float=0.0, 
                 go_ratio: int|float=50, amount: int=25):
         
         self.pressure = pressure
         self.temperature = temperature
         self.specific_gravity = specific_gravity
-        self.permeability_g = permeability_g
+        self.permeability = permeability
         self.skin = skin
         self.height_formation = height_formation
         self.well_radius = well_radius
@@ -24,59 +24,46 @@ class Darcy:
         self.go_ratio = go_ratio
         self.amount = amount
         
-    def _properties_gas_(self):
-        properties = GasProperties(self.pressure, self.temperature, self.specific_gravity)
-        mu_gas = properties.viscosity_gas()
-        z_gas = properties.factor_compressibility_gas()
-        return [mu_gas, z_gas]
+        self.delta_p = np.linspace(self.pressure, 14.7, self.amount)
         
-    def _p_diff_(self):
-        return np.linspace(self.pressure, 14.7, self.amount)
+        self._prop_gas = GasProperties(self.pressure, self.temperature, self.specific_gravity)
         
     def _a_flow_(self):
-        mu, z = self._properties_gas_()
-        A = ((1424*mu*z*self.temperature)/(self.permeability_g*self.height_formation))*(np.log(0.472*(self.reservoir_radius/self.well_radius))+self.skin)
-        return A
+        return ((1424*self._prop_gas.viscosity_gas()*self._prop_gas.factor_compressibility_gas()*self.temperature)/(self.permeability*self.height_formation))*(np.log(0.472*(self.reservoir_radius/self.well_radius))+self.skin)
         
-    def _b_flow_(self): 
-        z = self._properties_gas_()[1]        
-        beta = (2.33*1e10/(self.permeability_g**1.201))
-        B = (3.16e-12*beta*self.specific_gravity*z*self.temperature)/((self.height_formation**2)*self.well_radius)
-        return B
+    def _b_flow_(self):        
+        beta = (2.33*1e10/(self.permeability**1.201))
+        return (3.16e-12*beta*self.specific_gravity*self._prop_gas.factor_compressibility_gas()*self.temperature)/((self.height_formation**2)*self.well_radius)
         
-    def flow_gas(self):
-        p_diff = self._p_diff_()
-        flow_results = []
+    def _flow_gas_(self):
+        self._prop_gas = GasProperties(self.delta_p, self.temperature, self.specific_gravity)
         A = self._a_flow_()
         B = self._b_flow_()
-        
-        for p in p_diff:
-            flow = ((-A+np.sqrt((A**2)+(4*B*((self.pressure**2)-(p**2)))))/(2*B))
-            flow_results.append(flow)
-        
-        return np.array(flow_results)
+        return ((-A+np.sqrt((A**2)+(4*B*((self.pressure**2)-(self.delta_p**2)))))/(2*B))
     
     def inflow(self):
-        qg = self.flow_gas()
-        ql = ((self.go_ratio*qg)/(1-self.water_cut))/1000
-        qo = (1-self.water_cut)*ql
-        qw = ql-qo
-        pwf = self._p_diff_()
-        return [ql, qg, qo, qw, pwf]
+        qg = self._flow_gas_()
+        qo = qg/self.go_ratio
+        qw = (qg*self.water_cut*(1/self.go_ratio))/(1-self.water_cut)
+        ql = qw + qo
+        return [ql, qg/1000, qo, qw, self.delta_p]
     
-# if __name__ == "__main__":
+if __name__ == "__main__":
     
-#     ipr1 = Darcy(1309, 600, 0.673, 11, 9.95, 76, 0.3542, 909, 0.88, 48.6)
+    well = Darcy(1309, 600, 0.673, 11, 9.95, 76, 0.3542, 909, 0.88, 48.6)
 
-#     p = ipr1._p_diff_()
-#     q = ipr1.flow_gas()
-#     total = ipr1.inflow()
-#     print(total)
+    #print(well.flow_gas())
+    
+    # ql, qg, qo, qw, pw = well.inflow()
+    # print(ql, qg, qo, qw, pw)
+    
     # import matplotlib.pyplot as plt
-
-    # plt.plot(q, p)
-    # plt.title('IPR')
-    # plt.ylabel('Pwf(psia)')
-    # plt.xlabel('Qg(Mscf/D)')
+    
+    # fig, axs = plt.subplots(2, 2)
+    # axs[0, 0].plot(ql, pw)
+    # axs[0, 1].plot(qg, pw)
+    # axs[1, 0].plot(qo, pw)
+    # axs[1, 1].plot(qw, pw)
+    # #plt.plot(qo, pw)
     # plt.show()
 
